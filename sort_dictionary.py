1380b3343247751a2c96ee5169e34f8a077b78e5
飯島緋理 <iijimaakari@iijimaakari.local>
2018-05-31 11:33:26 +0900

    ver.8

--- sort_dictionary.py
+++ sort_dictionary.py
@@ -13,6 +13,8 @@ if len(dictionary[0]) == 1:
         char = ""
         for c in sorted(dictionary[i][0], key = str):
             char += c
+        if char.find("q") != -1:
+            char = char[:(char.find("u"))]+char[(char.find("u"))+1:]
         dictionary[i].append(char)
 sort_dictionary = sorted(dictionary, key=lambda x: x[1])
 
